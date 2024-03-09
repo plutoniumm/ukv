0b67e339dd96aed4129214fb91efd1d546d4123e
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2024-03-09 09:08:22 +0530

    init test

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 // Browser stuff
-export {
-  ls as LS,
-  ss as SS
-} from './browser';
-
 import { generate_api } from "./cfbulk";;
 import { generate_ns } from "./cfkv";;
 
-export const generate = (...something: any) => {
-  // check if KVNamespace
+const generate = (...something: any) => {
   if (something.length === 1 && something[0].get) {
     return generate_ns(something[0]);
   } else {
-    return generate_api(...something as ([string, string, string]));
+    return generate_api(...something as [string, string, string]);
   }
 };
+
+export { generate as CF };
+export { ls as LS, ss as SS } from './browser';
---
+++ test.ts
@@ -0,0 +1,2 @@
+import { LS, SS, CF } from './src/index';
+import { } from "./env.js"
